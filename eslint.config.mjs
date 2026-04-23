17a8ace59e3556de6c371339fa2fdeeb86570ec0
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2026-04-23 10:19:06 +0900

    Update eslint, prettier, husky, lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,26 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+    ...nextVitals,
+    ...nextTs,
+    {
+        rules: {
+            "react/display-name": "off",
+            
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/consistent-type-definitions": "off",
+            "@typescript-eslint/no-empty-function": "off",
+            "@typescript-eslint/no-unused-expressions": "off",
+        },
+    },
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+    ]),
 ]);
 
 export default eslintConfig;
