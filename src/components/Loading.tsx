79b624b1e7f29268cd6a2c58504e48461f66421f
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2026-04-23 13:34:30 +0900

    Apply Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,15 @@
+import { twMerge } from "tailwind-merge";
+
+export default function Loading({ className }: { className?: string }) {
+    return (
+        <div
+            className={twMerge(
+                "absolute top-0 left-0 w-full h-full flex items-center justify-center",
+                "bg-white bg-opacity-90 z-50",
+                className,
+            )}
+        >
+            <div className="w-16 h-16 border-t-2 border-b-2 border-gray-400 rounded-full animate-spin"></div>
+        </div>
+    );
+}
